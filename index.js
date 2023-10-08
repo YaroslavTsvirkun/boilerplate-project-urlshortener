f8191b70629b1a5e2aae6b706afd8b1e8ae710b1
YaroslavTsvirkun <yaroslav.tsvirkun.1992@outlook.com>
2023-10-08 11:12:18 +0000

    change index.js, added implementation URL Shortener Microservice

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient } = require('mongodb');
+const dns = require('dns');
+const urlparser = require('url');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const client = new MongoClient(process.env.DB_URL);
+const db = client.db('shorturls');
+const urls = db.collection('urls');
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,8 +23,38 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl/', function(req, res) {
+  const url = req.body.url;
+  const hostname = urlparser.parse(req.body.url).hostname;
+
+  async function checkHostname(err, address) {
+
+    if (!address) {
+      res.json({ error: 'Invalid Url' });
+    }
+    else {
+      const urlCount = await urls.countDocuments({});
+      const urlDoc = {
+        url: req.body.url,
+        short_url: urlCount
+      };
+
+      const result = await urls.insertOne(urlDoc);
+      console.log(result);
+      res.json({
+        original_url: url,
+        short_url: urlCount
+      });
+    }
+  }
+
+  dns.lookup(hostname, checkHostname);
+});
+
+app.get('/api/shorturl/:short_url', async function(req, res) {
+  let shorturl = req.params.short_url;
+  let urlDoc = await urls.findOne({ short_url: +shorturl });
+  res.redirect(urlDoc.url);
 });
 
 app.listen(port, function() {
